24aae97e0b8b363296ed8980dd25995f9416f4b9
khokon <k15981598k@gmail.com>
2021-10-29 01:36:01 +0600

    project structure complite

---
+++ components/header/Navigation.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+    }
+
+    navToggle = () => {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Navbar dark color="dark" expand="sm">
+                    <div className="container">
+                        <NavbarToggler onClick={this.navToggle} />
+                        <NavbarBrand href="/">Bohubrihi Restaurant</NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav className="mr-auto" navbar>
+                                <NavItem>
+                                    <Link to="/" className="nav-link active">Home</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/menu" className="nav-link">Menu</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/about" className="nav-link">About</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/contact" className="nav-link">Contact</Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
